24091e05f3171622b16d1b7b49b8dc4c4bc9f5e7
Bruno Capella <capella.bruno@gmail.com>
2015-12-22 16:16:50 -0200

    alterando classe template

--- DynamicSite.Entidades/Template.cs
+++ DynamicSite.Entidades/Template.cs
@@ -8,5 +8,21 @@ namespace DynamicSite.Entidades
 {
     public class Template
     {
+        public long Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Autor { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+        public decimal Versao { get; set; }
+
+        public bool PadraoSite { get; set; }
+
+        public bool EhAdministrativo { get; set; }
+
     }
 }
---
+++ DynamicSite.Infra/Configuration/TemplateConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using DynamicSite.Entidades;
+
+namespace DynamicSite.Infra.Configuration
+{
+    public class TemplateConfiguration : EntityTypeConfiguration<Template>
+    {
+        public TemplateConfiguration()
+        {
+            ToTable("TemplateSMS");
+            HasKey<long>(p => p.Id);
+            Property(p => p.Id).HasColumnName("IdTemplate").IsRequired();
+            Property(p => p.Nome).HasColumnType("VarChar").HasMaxLength(50).IsRequired();
+            Property(p => p.PadraoSite).HasColumnName("DefaultTemplate").IsOptional();
+            Property(p => p.Versao).HasPrecision(18, 10).IsOptional();
+            Property(p => p.Autor).HasMaxLength(100).IsRequired();
+            Property(p => p.DataCriacao).HasColumnType("DateTime").IsRequired();
+
+
+        }
+    }
+}
